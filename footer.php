0b7352c10504bf56b5926cd7f1606c2b5bb7b21d
SimasRabe <45881204+SimasRabe@users.noreply.github.com>
2018-12-19 00:40:51 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 	</body>
-	<footer class="footer">&copy; 2018 iif16/2 Rezervacijos sistema. All rights reserved</footer>
+	<footer class="footer">&copy; 2018 iif16/2 Rezervacijos Sistema. Visos teisės saugomos</footer>
 </html>
