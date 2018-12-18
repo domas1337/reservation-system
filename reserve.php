a9dbb709293dfef63dcd22d7b1b6186f07d8e88f
domas1337 <45968191+domas1337@users.noreply.github.com>
2018-12-18 22:31:27 +0200

    Update reserve.php

--- reserve.php
+++ reserve.php
@@ -4,7 +4,7 @@
 
 <section class="main-container">
 	<div class="main-wrapper">
-		<h2>Reserve</h2>
+		<h2>Rezervuoti</h2>
 		<?php
 			if (isset($_SESSION['userID'])) {
 				$name =  $_SESSION['userName'];
