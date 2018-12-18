7f06a8897cb4d6633eff2fe84ee93fdaa9fdbc42
domas1337 <45968191+domas1337@users.noreply.github.com>
2018-12-18 17:04:58 +0200

    Update editAccount.php

--- editAccount.php
+++ editAccount.php
@@ -6,22 +6,22 @@
 		<?php
 			if (isset($_SESSION['userID'])) {
 
-				echo '<h2>Edit account</h2>';
+				echo '<h2>Redaguoti paskyrą</h2>';
 				$id = $_SESSION['userID'];
 				$name =  $_SESSION['userName'];
 				$surname = $_SESSION['userSurname'];
 				$email = $_SESSION['userEmail'];
 				$phoneNumber = $_SESSION['userPhoneNumber'];
 				$role = $_SESSION['userRole'];
-				echo '<p>Logged in as '.$name.' '.$surname.'. Role: '.$role.'</p>';
+				echo '<p>Prisijungta kaip '.$name.' '.$surname.'. Rolė: '.$role.'</p>';
 				echo '<form class="signup-form" action="includes/editAccount.inc.php" method="POST">
-					<input type="text" name="userName" placeholder="Name" value="'.$name.'">
-					<input type="text" name="userSurname" placeholder="Surname" value="'.$surname.'">
-					<input type="text" name="userEmail" placeholder="E-mail" value="'.$email.'">
-					<input type="text" name="userPhoneNumber" placeholder="(+123) 123 12345" value="'.$phoneNumber.'">
-					<input type="password" name="userNewPassword" placeholder="New password (Optional)">
-					<input type="password" name="userPassword" placeholder="Current password">
-					<button type="submit" name="submit">Confirm</button>
+					<input type="text" name="userName" placeholder="Vardas" value="'.$name.'">
+					<input type="text" name="userSurname" placeholder="Pavardė" value="'.$surname.'">
+					<input type="text" name="userEmail" placeholder="Elektroninis paštas" value="'.$email.'">
+					<input type="text" name="userPhoneNumber" placeholder="(+370) 623 12345" value="'.$phoneNumber.'">
+					<input type="password" name="userNewPassword" placeholder="Naujas slaptažodis (Neprivaloma)">
+					<input type="password" name="userPassword" placeholder="Dabartinis slaptažodis">
+					<button type="submit" name="submit">Patvirtinti</button>
 				</form>';
 			} else
 				header("Location: index.php");
