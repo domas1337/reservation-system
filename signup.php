9a2940d0344010292139a699086593ac35f40e11
domas1337 <45968191+domas1337@users.noreply.github.com>
2018-12-18 17:08:08 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -6,16 +6,16 @@
 	<div class="main-wrapper">
 		<?php
 			if (isset($_SESSION['userID']))
-				echo '<h2>You already have an account</h2>';
+				echo '<h2>Jus jau turite paskyrą</h2>';
 			else {
-				echo '<h2>Sign up</h2>';
+				echo '<h2>Registruotis</h2>';
 				echo '<form class="signup-form" action="includes/signup.inc.php" method="POST">
-					<input type="text" name="userName" placeholder="First name">
-					<input type="text" name="userSurname" placeholder="Surname">
-					<input type="text" name="userEmail" placeholder="E-mail">
-					<input type="password" name="userPassword" placeholder="Password">
-					<input type="text" name="userPhoneNumber" placeholder="(+123) 123 12345">
-					<button type="submit" name="submit">Confirm</button>
+					<input type="text" name="userName" placeholder="Vardas">
+					<input type="text" name="userSurname" placeholder="Pavardė">
+					<input type="text" name="userEmail" placeholder="Elektroninis paštas">
+					<input type="password" name="userPassword" placeholder="Slaptažodis">
+					<input type="text" name="userPhoneNumber" placeholder="(+370) 623 12345">
+					<button type="submit" name="submit">Patvirtinti</button>
 				</form>';
 			}
 		?>
