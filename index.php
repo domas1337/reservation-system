cdaf03528701f4a96e299af9bfd21e6d5a6103f2
domas1337 <45968191+domas1337@users.noreply.github.com>
2018-12-18 22:30:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <section class="main-container">
 	<div class="main-wrapper">
-		<h2>Home</h2>
+		<h2>Pagrindinis</h2>
 		<?php
 			if (isset($_SESSION['userID'])) {
 				$name =  $_SESSION['userName'];
