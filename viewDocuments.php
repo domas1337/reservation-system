c674de3dce05d7babf30022e4e6b9bb0a178ba86
domas1337 <45968191+domas1337@users.noreply.github.com>
2018-12-18 22:35:30 +0200

    Update viewDocuments.php

--- viewDocuments.php
+++ viewDocuments.php
@@ -4,7 +4,7 @@
 
 <section class="main-container">
 	<div class="main-wrapper">
-		<h2>Documents</h2>
+		<h2>Dokumentai</h2>
 		<?php
 			if (isset($_SESSION['userID'])) {
 				$name =  $_SESSION['userName'];
