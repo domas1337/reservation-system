461de41fd87c0619cf63c05b96daf835dd57da18
domas1337 <45968191+domas1337@users.noreply.github.com>
2018-12-18 16:58:43 +0200

    Update adminAddReservation.php

--- adminAddReservation.php
+++ adminAddReservation.php
@@ -8,18 +8,18 @@
 
 				include_once 'includes/dbh.inc.php';
 
-				echo '<h2>Add account</h2>';
+				echo '<h2>Pridėti paskyrą</h2>';
 				$name =  $_SESSION['userName'];
 				$surname = $_SESSION['userSurname'];
 				$role = $_SESSION['userRole'];
-				echo '<p>Logged in as '.$name.' '.$surname.'. Role: '.$role.'</p>';
+				echo '<p>Prisijungta kaip '.$name.' '.$surname.'. Rolė: '.$role.'</p>';
 
 				echo '<form class="signup-form" action="includes/adminAddReservation.inc.php" method="POST">
-					<input type="text" name="roomName" placeholder="Reservation Name">
-					<input type="number" name="roomSize" placeholder="Reservation Size">
-					<input type="number" name="roomPrice" placeholder="Reservation Price">
-					<input type="text" name="roomPictureURL" placeholder="Reservation Picture URL">
-					<button type="submit" name="submit">Confirm</button>
+					<input type="text" name="roomName" placeholder="Rezervacijos pavadinimas">
+					<input type="number" name="roomSize" placeholder="Rezervacijos dydis">
+					<input type="number" name="roomPrice" placeholder="Rezervacijos kaina">
+					<input type="text" name="roomPictureURL" placeholder="Rezervacijos paveikslėlio URL">
+					<button type="submit" name="submit">Patvirtinti</button>
 				</form>';
 			} else
 				header("Location: index.php");
